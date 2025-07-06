8be9998522d5d88b0563bc2fa54063178f6fbd1d
Mikhail <mihaspb2007@gmail.com>
2025-07-06 20:12:49 +0400

    Update homework

--- 3. Send multiple GET requests/send_multiple_get_requests.py
+++ 3. Send multiple GET requests/send_multiple_get_requests.py
@@ -8,6 +8,29 @@ class TestCreateJokeCategory():
 
     base_url = "https://api.chucknorris.io/"
 
+    def test_random_joke(self, category: str):
+        url = self.base_url + "jokes/random"
+        params = {"category": category}
+        print(f"Requesting joke in category {category}")
+
+        response = requests.get(url , params = params)
+        print(f"Status code: {response.status_code}")
+        assert response.status_code == 200, (f"Expected status code 200, got {response.status_code}")
+        print("Status code is expected")
+
+        data = response.json()
+        print(json.dumps(data, ensure_ascii = False, indent = 2))
+
+        categories = data.get("categories", [])
+        print(f"Categories in response: {categories}")
+        assert isinstance(categories, list), (
+            f"'categories' should be a list, got {type(categories)}"
+        )
+        assert category in categories, (
+            f"Expected category '{category}' to be in {categories}"
+        )
+        print("Category is expected")
+
     def get_all_categories(self) -> List[str]:
         url = self.base_url + "jokes/categories"
         response = requests.get(url)
@@ -19,20 +42,10 @@ class TestCreateJokeCategory():
 
         return categories
 
-    def test_random_joke_for_each_category(self):
-        categories = self.get_all_categories()
-
-        for category in categories:
-            url = self.base_url + "jokes/random/"
-            params = {"category": category}
-            print(f"\nRequesting random joke in category {category} ")
-
-            response = requests.get(url, params=params)
-            assert response.status_code == 200, f"Expected status code 200. Got {response.status_code}"
-
-            joke = response.json()
-            print(json.dumps(joke, ensure_ascii = False, indent=2))
+    def test_all_category(self):
+        for category in self.get_all_categories():
+            self.test_random_joke(category)
 
 
 start = TestCreateJokeCategory()
-start.test_random_joke_for_each_category()
+start.test_all_category()
