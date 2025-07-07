5809f8be9bdddfb6a0e288c0e9b82be90a28133d
Mikhail <mihaspb2007@gmail.com>
2025-07-07 11:12:12 +0400

    Add homework: custom joke category selection

---
+++ 4. Send multiple GET requests with user menu/send_multiple_get_requests_with_menu.py
@@ -0,0 +1,56 @@
+import json
+import requests
+from typing import List
+
+
+class TestCreateJokeCategory():
+
+
+    base_url = "https://api.chucknorris.io/"
+
+    def test_random_joke(self, category: str):
+        url = self.base_url + "jokes/random"
+        params = {"category": category}
+        print(f"Requesting joke in category {category}")
+
+        response = requests.get(url , params = params)
+        print(f"Status code: {response.status_code}")
+        assert response.status_code == 200, (f"Expected status code 200, got {response.status_code}")
+        print("Status code is expected")
+
+        data = response.json()
+        print(json.dumps(data, ensure_ascii = False, indent = 2))
+
+        categories = data.get("categories", [])
+        print(f"Categories in response: {categories}")
+        assert isinstance(categories, list), (
+            f"'categories' should be a list, got {type(categories)}"
+        )
+        assert category in categories, (
+            f"Expected category '{category}' to be in {categories}"
+        )
+        print("Category is expected")
+
+    def get_all_categories(self) -> List[str]:
+        url = self.base_url + "jokes/categories"
+        response = requests.get(url)
+        assert response.status_code == 200, f"Expected status code 200. Got {response.status_code}"
+
+        categories = response.json()
+        assert isinstance(categories, list), f"Expected list of categories. Got {type(categories)}"
+        print(f"List of categories: {categories}")
+
+        return categories
+
+    def test_all_category(self):
+        for category in self.get_all_categories():
+            self.test_random_joke(category)
+
+
+start = TestCreateJokeCategory()
+user_category = input("Введите имя категории, для получения рандомной шутки из неё: ").strip()
+all_cats = start.get_all_categories()
+if user_category not in all_cats:
+    print(f"Категория '{user_category}' не существует. Существующие категории: {all_cats}")
+else:
+    start.test_random_joke(user_category)
